44d88495f54f73b84acc3e71a984eeced12abbae
Brett Casbeer <casbeebc@gmail.com>
2017-04-06 01:12:26 -0400

    Added send request method to client and cleaned up pypi package information

--- abenity/client.py
+++ abenity/client.py
@@ -1,9 +1,8 @@
 from abenity import exceptions
-
-try:
-    from urllib import request as http
-except ImportError:
-    import urllib2 as http
+import random
+import string
+import requests
+import json
 
 
 class Abenity(object):
@@ -23,15 +22,49 @@ class Abenity(object):
             environment: 'live' or 'sandbox'
             timeout: timeout in seconds
         """
+
         self._username = username
         self._password = password
         self._api_key = api_key
         self._version = int(version)
         self._environment = environment
+        self._api_url = 'https://api.abenity.com'
         if environment == 'sandbox':
-            self.api_url = 'https://sandbox.abenity.com'
+            self._api_url = 'https://sandbox.abenity.com'
         self._timeout = timeout
+
         self._public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC8NVUZUtr2I'+
         'HiFoY8s/qFGmZOIewAvgS4FMXWZ81Qc8lkAlZr9e171xn4PgKr+S7YsfCt+1XKyo5Xmr'+
         'JyaNUe/aRptB93NFn6RoFzExgfpkooxcHpWcPy+Hb5e0rwPDBA6zfyrYRj8uK/1HleFE'+
         'r4v8u/HbnJmiFoNJ2hfZXn6Qw== phpseclib-generated-key'
+
+        self._triple_des_key = random = ''.join([random.choice(
+            string.ascii_letters + string.digits) for n in range(24)])
+
+    def _send_request(self, http_method='GET', data={}):
+        params = dict('api_username': self.api_username,
+                      'api_password': self.api_password,
+                      'api_key': self.api_key,
+                      data.items())
+
+        api_url = self._api_url+'/v'+self._version+'/client'+http_method
+        headers = {'user-agent': 'abenity/abenity-php v2)'}
+        response = {}
+
+        if http_method == 'GET':
+            response = requests.get(api_url, verify=False, headers=headers
+                                    params=params, timeout=self._timeout)
+        elif http_method == 'POST':
+            response = requests.post(api_url, verify=False, headers=headers
+                                     params=params, timeout=self._timeout)
+
+        return json.loads(response.text)
+
+
+
+
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -3,6 +3,38 @@
 from setuptools import setup
 
 setup(
+    name='abenity-python',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',
+
+    description='Abenity API client',
+    long_description=open('README.md', 'r').read(),
+
+    # The project's main homepage.
+    url='https://github.com/casbeebc/abenity-python',
+
+    # Author details
+    author='Brett Casbeer',
+    author_email='brett.casbeer@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True,
 )
