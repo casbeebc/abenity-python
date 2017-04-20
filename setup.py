794e898e3a8d3f230ee3dfb55397c77a7800fb5f
Brett Casbeer <casbeebc@gmail.com>
2017-04-20 00:20:38 -0400

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 setup(
     name='abenity',
     packages=['abenity'],
-    version='0.0.1',
+    version='0.0.3',
 
     description='Abenity API client',
     long_description='A Python library for using the Abenity API.',
